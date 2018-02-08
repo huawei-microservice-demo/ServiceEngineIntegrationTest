7bdc2a66d1074ac548c32a96087ba9eb93263840
bao.liu <bao.liu@huawei.com>
2018-02-08 15:06:19 +0800

    undate script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-BASEDIR=/opt/CSE/code/cse-java-chassis/integration-tests
-JAVA_HOME=/opt/CSE/jdk1.8.0_144
+BASEDIR=/opt/liubao/code/ServiceEngineIntegrationTest
 
 cd ${BASEDIR}/account-service/target
-${JAVA_HOME}/bin/java -jar integration-tests-account-service-2.1.SNAPSHOT.jar >/dev/null 2>&1 &
+java -jar integration-tests-account-service-2.1.SNAPSHOT.jar >/dev/null 2>&1 &
 
 cd ${BASEDIR}/customer-service/target
-${JAVA_HOME}/bin/java -jar integration-tests-customer-service-2.1.SNAPSHOT.jar >/dev/null 2>&1 &
+java -jar integration-tests-customer-service-2.1.SNAPSHOT.jar >/dev/null 2>&1 &
 
 cd ${BASEDIR}/edge-service/target
-${JAVA_HOME}/bin/java -jar integration-tests-edge-service-2.1.SNAPSHOT.jar >/dev/null 2>&1 &
+java -jar integration-tests-edge-service-2.1.SNAPSHOT.jar >/dev/null 2>&1 &
 
 cd ${BASEDIR}/product-service/target
-${JAVA_HOME}/bin/java -jar integration-tests-product-service-2.1.SNAPSHOT.jar >/dev/null 2>&1 &
+java -jar integration-tests-product-service-2.1.SNAPSHOT.jar >/dev/null 2>&1 &
 
 cd ${BASEDIR}/tx-coordinator/target
-${JAVA_HOME}/bin/java -jar integration-tests-tx-coordinator-2.1.SNAPSHOT.jar >/dev/null 2>&1 &
+java -jar integration-tests-tx-coordinator-2.1.SNAPSHOT.jar >/dev/null 2>&1 &
 
 
 cd ${BASEDIR}/user-service/target
-${JAVA_HOME}/bin/java -jar integration-tests-user-service-2.1.SNAPSHOT.jar >/dev/null 2>&1 &
+java -jar integration-tests-user-service-2.1.SNAPSHOT.jar >/dev/null 2>&1 &
 
 count=1
 while ! nc -z localhost 8092 ; do
